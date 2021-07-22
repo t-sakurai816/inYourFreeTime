ff862df3a8021d81aa616984de59a77ba5e94a0e
t-sakurai816 <t.devsaku@gmail.com>
2021-07-22 13:10:39 +0900

    UPDATE: カードに情報を表示

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,8 +15,14 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col v-for="n in 24" :key="n" cols="4">
-            <v-card height="200"></v-card>
+          <v-col v-for="item in items" :key="item.id" cols="4">
+            <v-card height="200">
+              <h5>{{ item.title }}</h5>
+              <p>{{ item.desc }}</p>
+              <p>{{ item.userName }}</p>
+              <p>{{ item.age }}</p>
+              <p>{{ item.gender }}</p>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
@@ -25,13 +31,26 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "App",
 
   components: {},
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      items: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://8xop5xhioh.execute-api.ap-northeast-1.amazonaws.com/stg/items"
+      )
+      .then((response) => (this.items = response.data))
+      .catch((error) => {
+        console.log("ERR!", error);
+      });
+  },
 };
 </script>
