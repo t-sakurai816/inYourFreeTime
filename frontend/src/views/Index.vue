5f2d6f65043aa366b71814df6e7cd10f2f96d23e
yCroma <kuroyuki0728@gmail.com>
2021-07-23 18:26:16 +0900

    UPDATE: IndexとItemでページを分割

---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col v-for="item in items" :key="item.id" cols="4">
+        <ContentsCard :item="item" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import ContentsCard from "../components/ContentsCard.vue";
+export default {
+  name: "Index",
+
+  components: {
+    ContentsCard,
+  },
+
+  data() {
+    return {
+      items: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "https://8xop5xhioh.execute-api.ap-northeast-1.amazonaws.com/stg/items"
+      )
+      .then((response) => (this.items = response.data))
+      .catch((error) => {
+        console.log("ERR!", error);
+      });
+  },
+};
+</script>
