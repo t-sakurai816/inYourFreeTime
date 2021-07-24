6b04f028eee34edd4ca6fc1abf60b6acf2fbe209
t-sakurai816 <t.devsaku@gmail.com>
2021-07-24 13:56:09 +0900

    カードをクリックでページ遷移

--- frontend/src/components/ContentsCard.vue
+++ frontend/src/components/ContentsCard.vue
@@ -1,20 +1,39 @@
 <template>
-  <v-card>
-    <div class="title">{{ item.title }}</div>
-    <div class="desc">{{ item.desc }}</div>
-    <div class="user-name">{{ item.userName }}</div>
-    <div class="age">{{ item.age }}</div>
-    <div class="gender">{{ item.gender }}</div>
-    <div class="share-bar">
-      <v-icon>mdi-export-variant</v-icon>
-      <v-icon>mdi-twitter</v-icon>
+  <v-card
+    class="rounded-xl"
+    color="#ffffff"
+    hover="true"
+    link="true"
+    :to="{
+      name: 'Item',
+      params: { id: item.id },
+    }"
+  >
+    <div class="card">
+      <div class="title">
+        <h3>{{ item.title }}</h3>
+      </div>
+      <div class="desc">
+        <p>{{ item.desc }}</p>
+      </div>
+      <div class="user-name">
+        <p>{{ item.userName }}</p>
+      </div>
+      <div class="age">
+        <p>{{ item.age }}</p>
+      </div>
+      <div class="gender">
+        <p>{{ item.gender }}</p>
+      </div>
+      <div class="share-bar">
+        <v-icon>mdi-export-variant</v-icon>
+        <v-icon>mdi-twitter</v-icon>
+      </div>
     </div>
   </v-card>
 </template>
 
 <script>
-import { mdiExportVariant } from "@mdi/js";
-import { mdiTwitter } from "@mdi/js";
 export default {
   name: "ContentsCard",
   props: {
@@ -26,3 +45,13 @@ export default {
   item: {},
 };
 </script>
+
+<style scoped>
+/* カード全体のパディングを調整 */
+.card {
+  padding: 3% 10%;
+}
+.desc {
+  padding-top: 5%;
+}
+</style>
