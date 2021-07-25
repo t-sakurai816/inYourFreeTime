c22d3ae93dd6aca567dc2592b82ca4d61a760e70
yCroma <58209438+yCroma@users.noreply.github.com>
2021-07-25 12:48:01 +0900

    ADD: Vuelidate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from './plugins/vuetify'
+import Vuelidate from 'vuelidate'
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
@@ -9,7 +10,8 @@ import router from './router'
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  router,
-  render: (h) => h(App)
+	vuetify,
+	router,
+	Vuelidate,
+	render: (h) => h(App)
 }).$mount("#app");
