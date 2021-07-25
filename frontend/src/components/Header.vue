c0d10c9526dcd0bb949b753bb75919b3fcb56925
t-sakurai816 <t.devsaku@gmail.com>
2021-07-25 17:58:45 +0900

    header titleをリンクにした

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,7 +3,11 @@
     <v-app-bar app hide-on-scroll color="#ffffff">
       <v-spacer></v-spacer>
 
-      <v-toolbar-title>inYourFreeTime ~趣味の共有~</v-toolbar-title>
+      <v-toolbar-title>
+        <div class="header-title">
+          <a href="/">inYourFreeTime ~趣味の共有~</a>
+        </div>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
@@ -17,3 +21,10 @@ export default {
   name: "Header",
 };
 </script>
+
+<style scoped>
+.header-title a {
+  color: black;
+  text-decoration: none;
+}
+</style>
